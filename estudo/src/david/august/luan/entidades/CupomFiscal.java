a9cb397bf2e111a97358ca583b6fbf449fa910a7
Luan-Augusto <luanalencar134@gmail.com>
2020-09-27 19:30:20 -0300

    manutencao CumpomFiscal

--- estudo/src/david/august/luan/entidades/CupomFiscal.java
+++ estudo/src/david/august/luan/entidades/CupomFiscal.java
@@ -1,4 +1,41 @@
 package david.august.luan.entidades;
 
+import java.util.Random;
+
 public class CupomFiscal {
+    private Cliente cliente;
+    private Funcionario funcionario;
+    private Random codigo;
+
+    public CupomFiscal(Cliente cliente, Funcionario funcionario, Random codigo) {
+        this.cliente = cliente;
+        this.codigo = new Random();
+    }
+
+    // gerar um cod capaz de pegar 10 numeros aleatorios
+    public int codigoPedido(Random codigo) {
+        int cod = 0;
+        // para i menor que 10 incremente i
+        for (int i = 0; i < 10; i++) {
+            // armazenando a sequencia numerica na variavel cod
+            cod = codigo.nextInt();
+        }
+        return cod;// retorno
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Random getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Random codigo) {
+        this.codigo = codigo;
+    }
 }
--- estudo/src/david/august/luan/entidades/Pedido.java
+++ estudo/src/david/august/luan/entidades/Pedido.java
@@ -3,29 +3,17 @@ package david.august.luan.entidades;
 import java.util.*;
 
 public class Pedido {
-    private Random codigo;
+
     private String refeicao;
     private String bebida;
     private double preco;
     private Date dataPedido;
 
     public Pedido(String refeicao, String bebida, double preco) {
-        this.codigo = new Random();
         this.refeicao = refeicao;
         this.bebida = bebida;
         this.preco = preco;
         this.dataPedido = new Date();
-    }
-
-    // gerar um cod capaz de pegar 10 numeros aleatorios
-    public int codigoPedido(Random codigo) {
-        int cod = 0;
-        // para i menor que 10 incremente i
-        for (int i = 0; i < 10; i++) {
-            // armazenando a sequencia numerica na variavel cod
-            cod = codigo.nextInt();
-        }
-        return cod;// retorno
     }
 
     public String getrefeicao() {
