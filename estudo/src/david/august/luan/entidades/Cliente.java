4f574a03c2502bd8068ad8630379201da0adb122
Luan-Augusto <luanalencar134@gmail.com>
2020-09-27 19:09:36 -0300

    cadastrarCLiente() : Cliente

--- estudo/src/david/august/luan/entidades/Cliente.java
+++ estudo/src/david/august/luan/entidades/Cliente.java
@@ -1,8 +1,25 @@
 package david.august.luan.entidades;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cliente extends Pessoa {
+    private List<Cliente> clientes;
 
     public Cliente(String nome, String cpf) {
         super(cpf, nome);
+        this.clientes = new ArrayList<>();
+    }
+
+    public void cadastrarCliente(Cliente cliente) {
+        clientes.add(cliente);
+    }
+
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(List<Cliente> clientes) {
+        this.clientes = clientes;
     }
 }
--- estudo/src/david/august/luan/entidades/Fornecedor.java
+++
@@ -1,7 +0,0 @@
-package david.august.luan.entidades;
-
-public class Fornecedor extends Pessoa {
-    public Fornecedor(String cpf, String nome) {
-        super(cpf, nome);
-    }
-}
