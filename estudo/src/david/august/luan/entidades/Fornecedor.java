5e1f56db628a37f0014b749d579c6f349117c93d
Luan-Augusto <luanalencar134@gmail.com>
2020-09-27 18:50:41 -0300

    add construtor Fornecedor

---
+++ estudo/src/david/august/luan/entidades/Fornecedor.java
@@ -0,0 +1,7 @@
+package david.august.luan.entidades;
+
+public class Fornecedor extends Pessoa {
+    public Fornecedor(String cpf, String nome) {
+        super(cpf, nome);
+    }
+}
--- estudo/src/david/august/luan/entidades/Fornecedore.java
+++
@@ -1,4 +0,0 @@
-package david.august.luan.entidades;
-
-public class Fornecedore {
-}
